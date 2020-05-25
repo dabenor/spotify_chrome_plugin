23f13029711f6455ac0d83b36398cd14f4df4862
Amar Mujumdar <amujumdar@ucvts.tec.nj.us>
2020-05-25 15:59:50 -0400

    another nlp sentiment analysis

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 console.log("Hello World!");
 var Sentiment = require('sentiment');
 var sentiment = new Sentiment();
-var result = sentiment.analyze('I see quite a bit of bias in this statement.');
+var result = sentiment.analyze('I hate you. I love me. You are terrible. I am super happy. I love life. You suck ass.');
 console.dir(result);    // Score: -2, Comparative: -0.666
+
+var Analyzer = require('natural').SentimentAnalyzer;
+var stemmer = require('natural').PorterStemmer;
+var analyzer = new Analyzer("English", stemmer, "afinn");
+// getSentiment expects an array of strings
+console.log(analyzer.getSentiment(["I", "like", "cherries"]));
+// 0.6666666666666666
