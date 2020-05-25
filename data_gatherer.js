1245aa50a4b47cfde483d4eb81872f8d5361443c
dabenor <danielbenor@yahoo.com>
2020-05-25 15:00:41 -0400

    data gatherer initialization

--- data_gatherer.js
+++ data_gatherer.js
@@ -1,10 +1,14 @@
-function myFunction() {
+function get_webpage() {
     var bodyScripts = document.querySelectorAll("body script");
 
     for(var i=0; i<bodyScripts.length; i++){
         bodyScripts[i].remove();
     }
-    
+
     var str = document.body.textContent;
     document.body.innerHTML = '<pre>'+str+'</pre>';
+}
+
+function clean_data(){
+    
 }
