87fc86974189bcdb08718b546800a4d64217cdf9
Amar Mujumdar <amujumdar@ucvts.tec.nj.us>
2020-05-25 15:01:12 -0400

    bias

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 console.log("Hello World!");
 var Sentiment = require('sentiment');
 var sentiment = new Sentiment();
-var result = sentiment.analyze('Cats are stupid.');
+var result = sentiment.analyze('I see quite a bit of bias in this statement.');
 console.dir(result);    // Score: -2, Comparative: -0.666
