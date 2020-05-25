f6ab6d8725fac533174409f69b03dc5b54085ac2
Amar Mujumdar <amujumdar@ucvts.tec.nj.us>
2020-05-25 14:55:56 -0400

    sentiment analysis very beginning

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 console.log("Hello World!");
+var Sentiment = require('sentiment');
+var sentiment = new Sentiment();
+var result = sentiment.analyze('Cats are stupid.');
+console.dir(result);    // Score: -2, Comparative: -0.666
