041bb5e69807621f33c04cb27ceb44c9084e3b83
Amar Mujumdar <amujumdar@ucvts.tec.nj.us>
2020-05-25 21:15:33 -0400

    added bundle.js

--- main.js
+++ main.js
@@ -2,11 +2,4 @@ console.log("Hello World!");
 var Sentiment = require('sentiment');
 var sentiment = new Sentiment();
 var result = sentiment.analyze('I am not in danger, Skyler, I AM THE DANGER! A guy opens his door and you think that of me? I am the one who knocks!');
-console.dir(result);    // Score: -2, Comparative: -0.666
-
-var Analyzer = require('natural').SentimentAnalyzer;
-var stemmer = require('natural').PorterStemmer;
-var analyzer = new Analyzer("English", stemmer, "afinn");
-// getSentiment expects an array of strings
-console.log(analyzer.getSentiment(["I", "like", "cherries"]));
-// 0.6666666666666666
+console.log(result['comparative']);    // Score: -2, Comparative: -0.666
